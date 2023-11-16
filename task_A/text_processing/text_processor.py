a0ff967389a186b05c2e2322e3c0a10aa330d2e1
pmalesa <piotr.malesa96@gmail.com>
2023-11-16 23:06:42 +0100

    Add functionality to load the training data and to tokenize a single sentence.

--- task_A/text_processing/text_processor.py
+++ task_A/text_processing/text_processor.py
@@ -1,10 +1,14 @@
 import numpy as np
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 class TextProcessor:
 
     @staticmethod
-    def tokenize_text(text: str) -> str:
-        pass
+    def tokenize_text(text: str) -> list[str]:
+        doc = nlp(text)
+        return [token.text for token in doc]
 
     @staticmethod
     def create_embedding(text: str) -> np.ndarray:
