91a7d8321c6534ad5b5324211eb3f36d80f95447
pmalesa <piotr.malesa96@gmail.com>
2024-01-07 20:53:28 +0100

    Add chunk by chunk training.

--- models/generated_text_detection_model/generated_text_detection_model.py
+++ models/generated_text_detection_model/generated_text_detection_model.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer, TFAutoModel
 import tensorflow as tf
+import json
 
 from text_processing.text_processor import TextProcessor as tp
 
@@ -21,25 +22,57 @@ class GeneratedTextDetectionModel(tf.keras.Model):
         intermediate_output = self.__intermediate_dense_layer(cls_output)
         return self.__output_dense_layer(intermediate_output)
 
-    def train(self, texts: list[str], labels: list[int], learning_rate = 0.001, epochs = 10, batch_size = 16, chunk_size = 128, chunk_overlap = 32):
+    def train(self, texts: list[str], labels: list[int], learning_rate = 0.001, epochs = 10, chunk_size = 128, chunk_overlap = 32):
         if len(texts) != len(labels):
             print("[ERROR] Number of examples is not equal to the number of labels!")
             return
-        
+
+        # Compiling the model
         self.compile(
             optimizer = tf.keras.optimizers.Adam(learning_rate = learning_rate),
             loss = tf.keras.losses.BinaryCrossentropy(),
             metrics = [tf.keras.metrics.BinaryAccuracy()]
         )
 
+        # Training the model
         for i, (text, label) in enumerate(zip(texts, labels)):
-            self.__train_single(text, label, epochs, batch_size, chunk_size, chunk_overlap)
+            print(f"Example {i + 1}")
+            if i == 100:
+                print(f"[INFO] TOTAL EXAMPLES PROCESSED: {i}")
+                break
+            self.__train_single(text, label, epochs, chunk_size, chunk_overlap)
+            if i % 10 == 9:
+                print(f"[INFO] Examples processed so far: {i + 1}")
 
-    def __train_single(self, text: str, label: int, epochs: int, batch_size: int, chunk_size: int, chunk_overlap: int):
+    def load(self, path: str):
+        self = tf.keras.models.load_model(path)
+
+    def predict_text(self, text: str):
+        pass
+
+    def __train_single(self, text: str, label: int, epochs: int, chunk_size: int, chunk_overlap: int):
+        # Divide text into chunks
         text_chunks = tp.preprocess_text(text, chunk_size = chunk_size, chunk_overlap = chunk_overlap)
-        train_features = self.__tokenizer(text_chunks, padding = True, truncation = True, return_tensors = "tf")
-        train_labels = [label] * len(text_chunks)
-        train_labels_tensor = tf.constant(train_labels)
-        train_dataset = tf.data.Dataset.from_tensor_slices((dict(train_features), train_labels_tensor))
-        train_dataset = train_dataset.batch(batch_size)
-        self.fit(train_dataset, epochs = epochs)
+
+        # Tokenize all chunks at once
+        chunks_features = self.__tokenizer(text_chunks, padding = "max_length", truncation = True, max_length = 256, return_tensors = "tf")
+
+        for i in range(len(chunks_features["input_ids"])):
+            # Extract the tokenized information for a single chunk
+            chunk_features = {
+                "input_ids": tf.expand_dims(chunks_features["input_ids"][i], 0),
+                "attention_mask": tf.expand_dims(chunks_features["attention_mask"][i], 0),
+                "token_type_ids": tf.expand_dims(chunks_features["token_type_ids"][i], 0)
+            }
+
+            # Create single label tensor for the chunk
+            chunk_label = tf.constant([label])
+
+            # Create a dataset for a single chunk
+            chunk_dataset = tf.data.Dataset.from_tensor_slices((dict(chunk_features), chunk_label))
+            chunk_dataset = chunk_dataset.batch(1)
+
+            self.fit(chunk_dataset, epochs = epochs)
+
+    def __predict_single(self, text: str):
+        pass
--- training_module/training_module.py
+++ training_module/training_module.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime
 
 from io import StringIO
-from pandas import DataFrame, Series
+from pandas import DataFrame
 
 from text_processing.text_processor import TextProcessor as tp
 from models.generated_text_detection_model.generated_text_detection_model import GeneratedTextDetectionModel
@@ -24,29 +24,25 @@ class TrainingModule:
             print("[ERROR] Training data could not be loaded.")
             return
 
-        texts = self.__data["text"]
-        labels = self.__data["label"]
-
         # Model training
         gtd_model = GeneratedTextDetectionModel()
         learning_rate = 0.01
         epochs = 1
-        batch_size = 14
-        chunk_size = 64
-        chunk_overlap = 8
+        chunk_size = 128
+        chunk_overlap = 32
 
         start = datetime.now()
-        gtd_model.train(texts, labels, learning_rate, epochs, batch_size, chunk_size, chunk_overlap)
+        gtd_model.train(self.__data["text"], self.__data["label"], learning_rate, epochs, chunk_size, chunk_overlap)
         finish = datetime.now()
         duration = finish - start
-        training_time_in_s = duration.total_seconds() # [seconds]
+        training_time_in_s = duration.total_seconds()
 
         # Save model
         directory = "output/gtd"
         if not os.path.exists(directory):
             os.makedirs(directory, exist_ok = True)
         
-        model_name = f"model_weights-epochs({epochs})_batch_size({batch_size})_chunk_size({chunk_size})_chunk_overlap({chunk_overlap}).h5"
+        model_name = f"model_weights-epochs({epochs})_chunk_size({chunk_size})_chunk_overlap({chunk_overlap}).h5"
         model_save_path = os.path.join(directory, model_name)
         gtd_model.save_weights(model_save_path)
 
@@ -79,13 +75,9 @@ class TrainingModule:
 
         pass
 
-
-
     def __load_data(self, filename: str) -> DataFrame:
         with open(filename, "r") as file:
             jsonl_string = file.read()
             jsonl_io = StringIO(jsonl_string)
             return pd.read_json(jsonl_io, lines = True)
         
-    def __preprocess_text_data(self, texts_vec: Series) -> np.array:
-        pass
